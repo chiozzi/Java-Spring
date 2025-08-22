ee631045a5dff5a18fde353afe529d6f2cd5639f
Glauco Todesco <glauco.todesco@gmail.com>
2025-08-22 15:10:25 -0300

    Aula 22-08-2025

---
+++ src/main/java/com/fatec/itu/product/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.fatec.itu.product.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.itu.product.entities.Product;
+import com.fatec.itu.product.repositories.ProductRepository;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+ 
+    @Autowired
+    private ProductRepository repository;
+    
+    @GetMapping
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
+
+
+}
